c8b04e6938d6ae561d8e7ed5b96fc62f2a5b0801
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-26 21:15:44 +0530

    terraform_start

---
+++ 10-sg/main.tf
@@ -0,0 +1,34 @@
+
+# Using Open source module
+#https://github.com/terraform-aws-modules/terraform-aws-security-group/blob/master/README.md
+ 
+ module "catalogue" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "${local.common_name_suffix}-catalogue"
+  use_name_prefix = false # true vunte vere name will come
+  description = "Security group for catalogue with custom ports open within VPC, egress all traffic"
+  vpc_id      = data.aws_ssm_parameter.vpc_id.value # comes from data.tf
+
+} 
+
+
+# module "sg" {
+#   count = length(var.sg_names)
+#   source = "git::https://github.com/SrikanthErugula/terraform-AWS-VPC-module.git"
+#   project_name = var.project_name
+#   environment = var.environment
+#   sg_name = var.sg_names[count.index]
+#   sg_description = "Created for ${var.sg_names[count.index]}"
+#   vpc_id =  local.vpc_id
+# }
+
+# Frontend accepting traffic from frontend ALB
+# resource "aws_security_group_rule" "frontend_frontend_alb" {
+#   type              = "ingress"
+#   security_group_id = module.sg[9].sg_id # frontend SG ID
+#   source_security_group_id = module.sg[11].sg_id # frontend ALB SG ID
+#   from_port         = 80
+#   protocol          = "tcp"
+#   to_port           = 80
+# }
