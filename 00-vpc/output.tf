806bf226c3e80e0e8c836fa3bc3e7c99266061be
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-26 22:41:07 +0530

    terraform_start

--- 00-vpc/output.tf
+++ 00-vpc/output.tf
@@ -4,14 +4,14 @@ output "vpc_sri_id" {
 
 
 
-output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
-}
+# output "public_subnet_ids" {
+#   value = module.vpc.public_subnet_ids
+# }
 
-output "private_subnet_ids" {
-    value = module.vpc.public_subnet_ids
-}
+# output "private_subnet_ids" {
+#     value = module.vpc.public_subnet_ids
+# }
 
-output "database_subnet_ids" {
-    value = module.vpc.public_subnet_ids
-}
+# output "database_subnet_ids" {
+#     value = module.vpc.public_subnet_ids
+# }
