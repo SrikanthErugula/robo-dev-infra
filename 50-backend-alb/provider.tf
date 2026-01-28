1d2170e938350f52d13a44ca8c0c3823f789f70f
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-28 11:50:59 +0530

    terraform_start

---
+++ 50-backend-alb/provider.tf
@@ -0,0 +1,70 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "6.16.0"
+#     }
+#   }
+
+
+# # terraform {
+# #   backend "s3" {
+# #     bucket = "mybucket"
+# #     key    = "path/to/my/key"
+# #     region = "us-east-1"
+# #   }
+# # }
+
+#   backend "s3" {
+#     bucket = "dsoaws-remote-state"
+#     key    = "remote-state-demo" # key must be need set for storing state file ikkada ichhina name tho state file will be storing
+#     region = "us-east-1"
+#     use_lockfile = true  # it used to lock the state file when someone useing same time
+#     #dynamodb_table = "terraform-locks"
+#     encrypt = true
+#   }
+# }
+
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# # so here backend ni configure cheyakapothe state file ni local lo peduthundhi..
+
+# # adhe backed ni configure cheste adhi velli s3 bucket lo store avuthundhi...
+
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+
+  backend "s3" { # backend ante remote store ani anukovali
+    bucket = "dsoaws-remote-state"
+    key    = "dev-infra-backend-alb" # so here keys must be not for the same for all, u have to set name as per the requirement 
+    region = "us-east-1"
+    #use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+## backend ani pettakunda iste adhi local lo velli store avuthundhi...
+
+
+# actual CMD is as per doc
+
+# terraform {
+#   backend "s3" {
+#     bucket       = "example-bucket"
+#     key          = "path/to/state"
+#     use_lockfile = true
+#     region       = "us-east-1"
+#   }
+# }
