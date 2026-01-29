a4411c56a22517ed40c331f8485635191258c430
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-29 22:44:51 +0530

    terraform_start

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -5,11 +5,11 @@
 #   value = "bar"
 # } # SYNTAX for below code 
 
-# resource "aws_ssm_parameter" "vpc_id" {
-#   name  = "/${var.project_name}/${var.environment}/vpc_id"
-#   type  = "String"
-#   value = module.vpc_test.vpc_sri_id # comes from out.tf file
-# }
+resource "aws_ssm_parameter" "vpc_id" {
+  name  = "/${var.project_name}/${var.environment}/vpc_id"
+  type  = "String"
+  value = module.vpc_test.vpc_sri_id # comes from out.tf file
+}
 
 # resource "aws_ssm_parameter" "public_subnet_ids" {
 #   name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
