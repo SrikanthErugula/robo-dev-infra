e2ecd5c1aaf5eada77720600d91271446528b27e
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-29 09:47:31 +0530

    terraform_start

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -2,40 +2,41 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
 # SYNTAX take it from above URL
 
-# resource "aws_lb" "backend_alb" {
-#   name               = "${local.common_name_suffix}-backend-alb" # roboshop-dev-backend-alb
-#   internal           = true
-#   load_balancer_type = "application"
-#   security_groups    = [local.backend_alb_sg_id]
-#   # it should be private subnet ids
-#   subnets            = local.private_subnet_ids
+resource "aws_lb" "backend_alb" {
+  name               = "${local.common_name_suffix}-backend-alb" # roboshop-dev-backend-alb
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [local.backend_alb_sg_id]
+  # it should be private subnet ids
+  subnets            = local.private_subnet_ids
 
-#   enable_deletion_protection = false # prevents accidental deletion from UI
+  enable_deletion_protection = false # prevents accidental deletion from UI
 
-#   tags = merge (
-#     local.common_tags,
-#     {
-#         Name = "${local.common_name_suffix}-backend-alb"
-#     }
-#   )
-# }
+  tags = merge (
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-backend-alb"
+    }
+  )
+} # up to here lb done 
 
 # # Backend ALB listening on port number 80
-# resource "aws_lb_listener" "backend_alb" {
-#   load_balancer_arn = aws_lb.backend_alb.arn
-#   port              = "80"
-#   protocol          = "HTTP"
 
-#   default_action {
-#     type = "fixed-response"
+resource "aws_lb_listener" "backend_alb" {
+  load_balancer_arn = aws_lb.backend_alb.arn # see notes
+  port              = "80"
+  protocol          = "HTTP"
 
-#     fixed_response {
-#       content_type = "text/plain"
-#       message_body = "Hi, I am from backend ALB HTTP"
-#       status_code  = "200"
-#     }
-#   }
-# }
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Hi, I am from backend ALB HTTP"
+      status_code  = "200"
+    }
+  }
+}
 
 # resource "aws_route53_record" "backend_alb" {
 #   zone_id = var.zone_id
