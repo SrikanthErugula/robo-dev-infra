bb99eb1fe9d01c6f580d100bc875f4e27e733a85
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-30 13:09:00 +0530

    terraform_start

--- 00-vpc/output.tf
+++ 00-vpc/output.tf
@@ -4,14 +4,14 @@ output "vpc_sri_id" {
 
 
 
-# output "public_subnet_ids" {
-#   value = module.vpc.public_subnet_ids
-# }
+output "public_subnet_ids" {
+  value = module.vpc_test.public_subnet_ids
+}
 
-# output "private_subnet_ids" {
-#     value = module.vpc.public_subnet_ids
-# }
+output "private_subnet_ids" {
+    value = module.vpc_test.private_subnet_ids
+}
 
-# output "database_subnet_ids" {
-#     value = module.vpc.public_subnet_ids
-# }
+output "database_subnet_ids" {
+    value = module.vpc_test.database_subnet_ids
+}
--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -11,20 +11,22 @@ resource "aws_ssm_parameter" "vpc_id" {
   value = module.vpc_test.vpc_sri_id # comes from out.tf file
 }
 
-# resource "aws_ssm_parameter" "public_subnet_ids" {
-#   name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
-#   type  = "StringList"
-#   value = join("," , module.vpc.public_subnet_ids) # comes form output.tf
-# }
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/public_subnet_ids"
+  type  = "String"
+  value = join("," , module.vpc_test.public_subnet_ids) # comes from out.tf file
+}
 
-# resource "aws_ssm_parameter" "private_subnet_ids" {
-#   name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
-#   type  = "StringList"
-#   value = join("," , module.vpc.private_subnet_ids)
-# }
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/private_subnet_ids"
+  type  = "String"
+  value = join("," , module.vpc_test.private_subnet_ids) # comes from out.tf file
+}
 
-# resource "aws_ssm_parameter" "database_subnet_ids" {
-#   name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
-#   type  = "StringList"
-#   value = join("," , module.vpc.database_subnet_ids)
-# }
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project_name}/${var.environment}/database_subnet_ids"
+  type  = "String"
+  value = join("," , module.vpc_test.database_subnet_ids) # comes from out.tf file
+}
+
+
